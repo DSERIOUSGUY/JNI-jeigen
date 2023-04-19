c4547f78743adc6452e2cc8507778fef0ef0e933
DSERIOUSGUY <utsavagarwal2014@gmail.com>
2023-04-19 01:39:45 +0100

    Adding native code formatting for JNI

--- src/native/jeigen.cpp
+++ src/native/jeigen.cpp
@@ -6,6 +6,9 @@
 
 #include <stdexcept>
 #include <iostream>
+#include <jni.h>
+#include <android/log.h>
+
 using namespace std;
 
 #include "jeigen.h"
@@ -16,6 +19,12 @@ using namespace std;
 #include "Eigen/Eigenvalues"
 #include "unsupported/Eigen/MatrixFunctions"
 using namespace Eigen;
+
+extern "C" {
+    JNIEXPORT jint JNICALL Java_com_example_datacollection_jeigen_myNativeMethod(JNIEnv *env, jclass clazz, jint arg) {
+        return arg * arg;
+    }
+}
 
 
 /* use Map intead of this method
@@ -55,35 +64,53 @@ SparseMatrix<double> *getSparseMatrix_(int handle ) {
     return (SparseMatrix<double> *)(data[handle]);
 }
 
+int print_log(string tag, string message){
+//    const char *tag_ptr = tag.c_str();
+    return __android_log_print(ANDROID_LOG_DEBUG, "test", "%s", message.c_str());
+}
+
 extern "C" {
-    DllExport void init() {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_init(JNIEnv *env) {
         for( int i = 0; i < RESULTS_SIZE; i++ ) {
             data[i] = 0;
         }
     }
-    DllExport int allocateSparseMatrix( int numEntries, int numRows, int numCols, int *rows, int *cols, double *values ) {
+    JNIEXPORT jint JNICALL Java_com_example_datacollection_jeigen_allocateSparseMatrix( JNIEnv *env, jclass clazz, jint numEntries, jint numRows, jint numCols, jintArray rows_arr, jintArray cols_arr, jdoubleArray values_arr ) {
         SparseMatrix<double> *pmat = new SparseMatrix<double>(numRows, numCols);
         pmat->reserve(numEntries);
         typedef Eigen::Triplet<double> T;
         std::vector<T> tripletList;
+
+        jint* rows = env->GetIntArrayElements(rows_arr, JNI_FALSE);
+        jint* cols = env->GetIntArrayElements(cols_arr,JNI_FALSE);
+        jdouble* values = env->GetDoubleArrayElements(values_arr,JNI_FALSE);
+//        print_log("test","hello2");
+
+
+
         for( int i = 0; i < numEntries; i++ ) {
             tripletList.push_back(T(rows[i],cols[i],values[i]));
         }
         pmat->setFromTriplets(tripletList.begin(), tripletList.end() );
         return storeData_(pmat);
     }
-    DllExport void getSparseMatrixStats( int handle, int* stats ) { // rows, cols, nonzero
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_getSparseMatrixStats( JNIEnv *env, jclass clazz, jint handle, jint* stats ) { // rows, cols, nonzero
         stats[0] = getSparseMatrix_(handle)->rows();
         stats[1] = getSparseMatrix_(handle)->cols();
         stats[2] = getSparseMatrix_(handle)->nonZeros();
     }
-    DllExport int getSparseMatrixNumEntries( int handle ) {
+    JNIEXPORT jint JNICALL Java_com_example_datacollection_jeigen_getSparseMatrixNumEntries( int handle ) {
         return getSparseMatrix_(handle)->nonZeros();
     }
-    DllExport void getSparseMatrix( int handle, int *rows, int *cols, double *values ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_getSparseMatrix( JNIEnv *env, jclass clazz, jint handle, jintArray rows_arr, jintArray cols_arr, jdoubleArray values_arr ) {
         SparseMatrix<double> *pmat = getSparseMatrix_(handle);
         int numEntries = pmat->nonZeros();
         int i = 0;
+
+        jint* rows = env->GetIntArrayElements(rows_arr, JNI_FALSE);
+        jint* cols = env->GetIntArrayElements(cols_arr,JNI_FALSE);
+        jdouble* values = env->GetDoubleArrayElements(values_arr,JNI_FALSE);
+
         for (int k=0; k<pmat->outerSize(); ++k) {
             for (SparseMatrix<double>::InnerIterator it(*pmat,k); it; ++it) {
                 rows[i] = it.row();
@@ -93,7 +120,7 @@ extern "C" {
             }
       }
     }
-    DllExport void freeSparseMatrix( int handle ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_freeSparseMatrix( JNIEnv *env, jclass clazz, jint handle ) {
         if( handle < 0 || handle >= RESULTS_SIZE ) {
             throw std::runtime_error("handle out of range");
         }
@@ -101,30 +128,44 @@ extern "C" {
         data[handle] = 0;
     }
     // dummy operation to measure end to end latency
-    DllExport void dense_dummy_op1( int rows, int cols, double *afirst, double *aresult ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_dense_dummy_op1( JNIEnv *env, jclass clazz, jint rows, jint cols, jdoubleArray afirst_arr, jdoubleArray aresult_arr ) {
+
+        jdouble* afirst = env->GetDoubleArrayElements(afirst_arr, JNI_FALSE);
+        jdouble* aresult = env->GetDoubleArrayElements(aresult_arr,JNI_FALSE);
+
         Map<MatrixXd> first(afirst,rows,cols);
         Map<MatrixXd> result(aresult,rows,cols);
     }
     // dummy operation to measure end to end latency
-    DllExport void dense_dummy_op2( int rows, int middle, int cols, double *afirst, double *asecond, double *aresult ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_dense_dummy_op2( JNIEnv *env, jclass clazz, jint rows, jint middle, jint cols, jdoubleArray afirst_arr, jdoubleArray asecond_arr, jdoubleArray aresult_arr ) {
+
+        jdouble* afirst = env->GetDoubleArrayElements(afirst_arr, JNI_FALSE);
+        jdouble* aresult = env->GetDoubleArrayElements(aresult_arr,JNI_FALSE);
+        jdouble* asecond = env->GetDoubleArrayElements(asecond_arr, JNI_FALSE);
+
         Map<MatrixXd>first(afirst,rows,middle);
         Map<MatrixXd>second(asecond,middle,cols);
         Map<MatrixXd>result(aresult,rows,cols);
     }
-    DllExport void dense_multiply( int rows, int middle, int cols, double *afirst, double *asecond, double *aresult ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_dense_multiply( JNIEnv *env, jclass clazz, jint rows, jint middle, jint cols, jdoubleArray afirst_arr, jdoubleArray asecond_arr, jdoubleArray aresult_arr ) {
+
+        jdouble* afirst = env->GetDoubleArrayElements(afirst_arr, JNI_FALSE);
+        jdouble* aresult = env->GetDoubleArrayElements(aresult_arr,JNI_FALSE);
+        jdouble* asecond = env->GetDoubleArrayElements(asecond_arr, JNI_FALSE);
+
         Map<MatrixXd>first(afirst,rows,middle);
         Map<MatrixXd>second(asecond,middle,cols);
         Map<MatrixXd>result(aresult,rows,cols);
         result = first * second;
     }
-    DllExport int sparse_multiply( int rows, int middle, int cols,
-        int onehandle, int twohandle ) {
+    JNIEXPORT jint JNICALL Java_com_example_datacollection_jeigen_sparse_multiply( JNIEnv *env, jclass clazz, jint rows, jint middle, jint cols,
+        jint onehandle, jint twohandle ) {
         SparseMatrix<double> *presult = new SparseMatrix<double>(rows,cols);
         *presult = (*getSparseMatrix_(onehandle)) * (*getSparseMatrix_(twohandle));
         return storeData_(presult);
     }
-    DllExport int sparse_dummy_op2( int rows, int middle, int cols,
-        int onehandle, int twohandle, int numResultColumns ) {
+    JNIEXPORT jint JNICALL Java_com_example_datacollection_jeigen_sparse_dummy_op2( JNIEnv *env, jclass clazz, jint rows, jint middle, jint cols,
+        jint onehandle, jint twohandle, jint numResultColumns ) {
         SparseMatrix<double> *presult = new SparseMatrix<double>(rows,cols);
         presult->reserve(numResultColumns*rows);
         typedef Eigen::Triplet<double> T;
@@ -137,29 +178,53 @@ extern "C" {
         presult->setFromTriplets(tripletList.begin(), tripletList.end() );
         return storeData_(presult);
     }
-    DllExport void sparse_dense_multiply( int rows, int middle, int cols, int onehandle, double *asecond, double *aresult ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_sparse_dense_multiply( JNIEnv *env, jclass clazz, jint rows, jint middle, jint cols, jint onehandle, jdoubleArray asecond_arr, jdoubleArray aresult_arr ) {
+
+        jdouble* aresult = env->GetDoubleArrayElements(aresult_arr,JNI_FALSE);
+        jdouble* asecond = env->GetDoubleArrayElements(asecond_arr, JNI_FALSE);
+
         Map<MatrixXd> second(asecond,middle,cols);
         Map<MatrixXd> result(aresult,rows,cols);
         result = (*getSparseMatrix_(onehandle)) * second;
     }
-    DllExport void dense_sparse_multiply( int rows, int middle, int cols, double *afirst, int twohandle, double *aresult ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_dense_sparse_multiply( JNIEnv *env, jclass clazz, jint rows, jint middle, jint cols, jdoubleArray afirst_arr, jint twohandle, jdoubleArray aresult_arr ) {
+
+        jdouble* afirst = env->GetDoubleArrayElements(afirst_arr,JNI_FALSE);
+        jdouble* aresult = env->GetDoubleArrayElements(aresult_arr, JNI_FALSE);
+
         Map<MatrixXd> first(afirst, rows,middle);
         Map<MatrixXd> result(aresult,rows,cols);
         result =  first * (*getSparseMatrix_(twohandle));
     }
-    DllExport void ldlt_solve( int arows, int acols, int bcols, double *avalues, double *bvalues, double *xvalues ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_ldlt_solve( JNIEnv *env, jclass clazz, jint arows, jint acols, jint bcols, jdoubleArray avalues_arr, jdoubleArray bvalues_arr, jdoubleArray xvalues_arr ) {
+
+        jdouble* avalues = env->GetDoubleArrayElements(avalues_arr,JNI_FALSE);
+        jdouble* bvalues = env->GetDoubleArrayElements(bvalues_arr, JNI_FALSE);
+        jdouble* xvalues = env->GetDoubleArrayElements(xvalues_arr, JNI_FALSE);
+
         Map<MatrixXd> A(avalues,arows, acols);
         Map<MatrixXd> b(bvalues, arows, bcols);
         Map<MatrixXd> result(xvalues, acols, bcols);
         result = A.ldlt().solve(b);
     }
-    DllExport void fullpivhouseholderqr_solve( int arows, int acols, int bcols, double *avalues, double *bvalues, double *xvalues ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_fullpivhouseholderqr_solve( JNIEnv *env, jclass clazz, jint arows, jint acols, jint bcols, jdoubleArray avalues_arr, jdoubleArray bvalues_arr, jdoubleArray xvalues_arr ) {
+
+        jdouble* avalues = env->GetDoubleArrayElements(avalues_arr,JNI_FALSE);
+        jdouble* bvalues = env->GetDoubleArrayElements(bvalues_arr, JNI_FALSE);
+        jdouble* xvalues = env->GetDoubleArrayElements(xvalues_arr, JNI_FALSE);
+
         Map<MatrixXd> A(avalues, arows, acols);
         Map<MatrixXd> b(bvalues, arows, bcols);
         Map<MatrixXd> result(xvalues, acols, bcols);
         result = A.fullPivHouseholderQr().solve(b);
     }
-    DllExport void svd_dense( int n, int p, double *in, double *u, double *s, double *v ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_svd_dense( JNIEnv *env, jclass clazz, jint n, jint p, jdoubleArray in_arr, jdoubleArray u_arr, jdoubleArray s_arr, jdoubleArray v_arr ) {
+
+        jdouble* in = env->GetDoubleArrayElements(in_arr,JNI_FALSE);
+        jdouble* u = env->GetDoubleArrayElements(u_arr, JNI_FALSE);
+        jdouble* s = env->GetDoubleArrayElements(s_arr, JNI_FALSE);
+        jdouble* v = env->GetDoubleArrayElements(v_arr, JNI_FALSE);
+
         int m = min( n,p);
         Map<MatrixXd> In(in, n, p );
         JacobiSVD<MatrixXd,HouseholderQRPreconditioner> svd(In, ComputeThinU | ComputeThinV);
@@ -169,7 +234,14 @@ extern "C" {
         }
         matrixToValues( p, m, &(svd.matrixV()), v );
     }
-    DllExport void jeigen_eig( int n, double* in, double* values_real, double *values_imag, double* vectors_real, double *vectors_imag ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_jeigen_eig( JNIEnv *env, jclass clazz, jint n, jdoubleArray in_arr, jdoubleArray values_real_arr, jdoubleArray values_imag_arr, jdoubleArray vectors_real_arr, jdoubleArray vectors_imag_arr ) {
+
+        jdouble* in = env->GetDoubleArrayElements(in_arr,JNI_FALSE);
+        jdouble* values_real = env->GetDoubleArrayElements(values_real_arr, JNI_FALSE);
+        jdouble* values_imag = env->GetDoubleArrayElements(values_imag_arr, JNI_FALSE);
+        jdouble* vectors_real = env->GetDoubleArrayElements(vectors_real_arr, JNI_FALSE);
+        jdouble* vectors_imag = env->GetDoubleArrayElements(vectors_imag_arr, JNI_FALSE);
+
         Map<MatrixXd> In( in, n, n );
         EigenSolver<MatrixXd> eigenSolve( In );
         VectorXcd EigenValues = eigenSolve.eigenvalues();
@@ -189,7 +261,13 @@ extern "C" {
             }
         }
     }
-    DllExport void jeigen_peig( int n, double* in, double* eigenValues, double* eigenVectors ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_jeigen_peig( JNIEnv *env, jclass clazz, jint n, jdoubleArray in_arr, jdoubleArray eigenValues_arr, jdoubleArray eigenVectors_arr ) {
+
+        jdouble* in = env->GetDoubleArrayElements(in_arr,JNI_FALSE);
+        jdouble* eigenValues = env->GetDoubleArrayElements(eigenValues_arr, JNI_FALSE);
+        jdouble* eigenVectors = env->GetDoubleArrayElements(eigenVectors_arr, JNI_FALSE);
+
+
         Map<MatrixXd> In( in, n, n );
         EigenSolver<MatrixXd> eigenSolve( In );
         MatrixXd EigenValues = eigenSolve.pseudoEigenvalueMatrix();
@@ -197,14 +275,25 @@ extern "C" {
         matrixToValues( n, n, &(EigenValues), eigenValues );
         matrixToValues( n, n, &(EigenVectors), eigenVectors );
     }
-    DllExport void jeigen_exp( int n, double *in, double *result ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_jeigen_1exp( JNIEnv *env, jclass clazz, jint n, jdoubleArray in_arr, jdoubleArray result_arr ) {
+
+        jdouble* in = env->GetDoubleArrayElements(in_arr,JNI_FALSE);
+        jdouble* result = env->GetDoubleArrayElements(result_arr, JNI_FALSE);
+
         Map<MatrixXd> In(in, n, n );
         Map<MatrixXd> Result(result,n,n);
         Result = In.exp();
+
+//        print_log("result","expo:"+to_string(Result.cols()));
     }
-    DllExport void jeigen_log(int n, double *in, double *result ) {
+    JNIEXPORT void JNICALL Java_com_example_datacollection_jeigen_jeigen_log(JNIEnv *env, jclass clazz, jint n, jdoubleArray in_arr, jdoubleArray result_arr ) {
+
+        jdouble* in = env->GetDoubleArrayElements(in_arr,JNI_FALSE);
+        jdouble* result = env->GetDoubleArrayElements(result_arr, JNI_FALSE);
+
         Map<MatrixXd> In(in, n, n );
         Map<MatrixXd> Result(result,n,n);
         Result = In.log();
     }
 } // extern "C"
+
